4f1bc1a203df06612ba8ae13b39e117030d0500a
Daniel Reardon <drdanreardon@gmail.com>
2011-11-18 08:58:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbpost::Application.config.secret_token = 'ba5b9aa07f6a685c815a11c7ac779e4d7a0b8dd4c5c543a26134f1639b948647c8a7c8bff50b549f694457313e336470197b9f316a15dcd1cccc85cffbd121f8'
